e08158b766b189b1bfb73155d138079df46f95ee
Anmol Goel <agoel00@gmail.com>
2018-11-19 18:44:37 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import numpy as np
+
+app = Flask(__name__)
+global clf
+
+def getParameters():
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    cigsPerDay = request.args.get('cigs')
+    totChol = request.args.get('chol')
+    sysBP = request.args.get('sBP')
+    diabetes = request.args.get('dia')
+    diaBP = request.args.get('dBP')
+    glucose = request.args.get('gluc')
+    heartRate = request.args.get('hRate')
+
+    params = {
+        'age': age,
+        'sex': sex,
+        'cigsPerDay': cigsPerDay,
+        'totChol': totChol,
+        'sysBP': sysBP,
+        'diabetes': diabetes,
+        'diaBP': diaBP,
+        'glucose': glucose,
+        'heartRate': heartRate
+    }
+    return (params)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    params = getParameters()
+    input = np.array(
+        [[
+        int(params['age']),
+        int(params['sex']),
+        int(params['cigsPerDay']),
+        int(params['totChol']),
+        int(params['sysBP']),
+        int(params['diabetes']),
+        int(params['diaBP']),
+        int(params['glucose']),
+        int(params['heartRate'])
+    ]]
+    )
+    prediction = (clf.predict(input)).tolist()
+    probability = (clf.predict_proba(input)).tolist()
+    return jsonify(
+        {
+            'probability': probability,
+            'prediction': prediction,
+            'data': params
+        }
+    )
+
+@app.route('/model')
+def model():
+    coefficients = clf.coef_.tolist()
+    intercept = clf.intercept_.tolist()
+    return jsonify(
+        {
+            'coefficients': coefficients,
+            'intercept': intercept
+        }
+    )
+
+if __name__ == '__main__':
+    clf = joblib.load('./model/logreg.pkl')
+    app.run(debug=True)
