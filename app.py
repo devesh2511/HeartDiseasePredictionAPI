66a6ee98e79feda5b546222d4ea7640c5dd94f3a
Devesh Jain <76024137+devesh2511@users.noreply.github.com>
2023-03-02 23:32:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,12 +82,11 @@ def model():
 
 @app.route('/')
 def index():
-    return('<h1> JSON API for predicting Coronary Heart Disease in a patient. </h1>'+
-            '<h2> An example query is <a href="https://heartapi.herokuapp.com/predict?age=31&sex=1&cigs=5&chol=230&sBP=280&dia=0&dBP=90&gluc=87&hRate=84">this</a></h2>'+
+    return('<h1> Rest API for predicting Coronary Heart Disease in a patient created by <a href="https://github.com/devesh2511">Devesh</a></p> </h1>'+
+            '<h2> An example query is <a href="https://heartapi-dev.onrender.com/predict?age=31&sex=1&cigs=5&chol=230&sBP=280&dia=0&dBP=90&gluc=87&hRate=84">this</a></h2>'+
             '<h3>It uses parameters such as Age, Sex, Blood Pressure, Diabetes, Glucose and Heart rate for prediction. </h3>'+
             '<h3> This api uses scikit-learn for training the ML model and Flask for serving the webservice. It is trained on a Logistic Regression model with ~88% accuracy</h3>'+
-            '<p>For more details view <a href="https://github.com/agoel00/HeartDiseasePredictionAPI">source</a></p>' +
-            '<p>Made with 💙 by <a href="https://github.com/agoel00">Anmol Goel</a></p>')
+            '<p>For more details view <a href="https://github.com/devesh2511/HeartDiseasePredictionAPI">source</a></p>')
 
 if __name__ == '__main__':
     print('Model loaded successfully!')
